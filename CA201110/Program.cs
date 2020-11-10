e86f320f6399ad8a35a0cbcc4d214ab340cad800
Kiss Bence <kiss.bence@vereb.dc>
2020-11-10 10:10:12 +0100

    Add project files.

---
+++ CA201110/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CA201110
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+
+
+
+
+
+        }
+    }
+}
